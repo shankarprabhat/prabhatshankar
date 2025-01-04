3dea37a7b60514b63138193bbf807b02499d4843
prabhat shankar <prabhat4686@gmail.com>
2025-01-04 19:48:16 +0530

    updated register.js with mongo details

--- api/register.js
+++ api/register.js
@@ -1,4 +1,28 @@
+const mongoose = require('mongoose');
+
+const uri = 'mongodb+srv://prabhat4686:iKN_QTu4TAxb966@cluster0.1ke96.mongodb.net/data-science-coaching'; 
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Error connecting to MongoDB:", err));
+
+// Define a schema and model
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    mobile: String,
+    profession: String,
+    additional: String,
+    paymentStatus: String
+});
+
+const User = mongoose.model('User', userSchema,'ds-beginner-offline');
+
+
+
 // API to handle form submissions
+
+
+
 module.exports = async (req, res) => { 
     const { name, email, mobile, profession, additional, paymentStatus} = req.body;
 
